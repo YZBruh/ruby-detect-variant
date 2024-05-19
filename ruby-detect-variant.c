3c0e4e5ef192adb37d45ff5a0fc881a059bdd852
YZBruh <yagizzengin73@gmail.com>
2024-05-19 13:50:19 +0300

    ruby-detect-variant: add license text

--- ruby-detect-variant.c
+++ ruby-detect-variant.c
@@ -1,5 +1,21 @@
 /* ruby-detect-variant | ruby-set-variant.c */
 
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #define __RUBY_VARIANT_SET_INCLUDED__
 #define __IS_USES_LOGGING_FEATURES__
 
