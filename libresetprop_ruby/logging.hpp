b5d15ba88e676fbb676d71c8edb4c4554c56f0ac
Yağız Zengin <yagizzengin73@gmail.com>
2024-06-11 23:05:22 +0300

    libresetprop_ruby: fix building error

--- libresetprop_ruby/logging.hpp
+++ libresetprop_ruby/logging.hpp
@@ -5,14 +5,6 @@
 #include <sys/cdefs.h>
 #include <android/log.h>
 
-struct log_callback {
-    int (*d)(const char* fmt, va_list ap);
-    int (*i)(const char* fmt, va_list ap);
-    int (*w)(const char* fmt, va_list ap);
-    int (*e)(const char* fmt, va_list ap);
-    void (*ex)(int code);
-};
-
 extern log_callback log_cb;
 
 #define LIBRESETPROP_RUBY "libresetprop_ruby"
